f34b9b51c0ec325071a4dfb4fad1a2bf59bf2374
sid-rajaram <sid@placed.com>
2014-08-01 15:44:08 -0700

    added box boundaries for 5 states to llbounds

--- ll_bounds.py
+++ ll_bounds.py
@@ -30,8 +30,42 @@ puget_sound_pts['S'] = 46.5
 puget_sound_pts['E'] = -121.5
 puget_sound_pts['W'] = -123.5
 
+washington_pts = dict()
+washington_pts['N'] = 45.55
+washington_pts['S'] = 49
+washington_pts['E'] = -116.916
+washington_pts['W'] = -124.77
+
+texas_pts = dict()
+texas_pts['N'] = 25.83
+texas_pts['S'] = 36.5
+texas_pts['E'] = -93.5
+texas_pts['W'] = -106.65
+
+newyork_pts = dict()
+newyork_pts['N'] = 40.5
+newyork_pts['S'] = 45.017
+newyork_pts['E'] = -71.85
+newyork_pts['W'] = -79.77
+
+california_pts = dict()
+california_pts['N'] = 32.53
+california_pts['S'] = 42
+california_pts['E'] = -114.13
+california_pts['W'] = -124.44
+
+kansas_pts = dict()
+kansas_pts['N'] = 37
+kansas_pts['S'] = 40
+kansas_pts['E'] = -94.58
+kansas_pts['W'] = -102.05
+
 alaska_poly = poly_from_bounds(alaska_pts)
 hawaii_poly = poly_from_bounds(hawaii_pts)
 mainland_poly = poly_from_bounds(mainland_pts)
 puget_sound_poly = poly_from_bounds(puget_sound_pts)
-
+washington_poly = poly_from_bounds(washington_pts)
+texas_poly = poly_from_bounds(texas_pts)
+newyork_poly = poly_from_bounds(newyork_pts)
+california_poly = poly_from_bounds(california_pts)
+kansas_poly = poly_from_bounds(kansas_pts)
